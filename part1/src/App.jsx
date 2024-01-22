38b83689962f673022c833028f3bef335e5845cd
AlexGarcesIsAlreadyTaken <alex.garces@estudiantat.upc.edu>
2024-01-23 00:03:47 +0100

    part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,56 @@
+
+const Header = (props) => {
+  console.log(props.course)
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+
+const Part = (props) => {
+  console.log(props);
+  return(
+    <p>{props.part} {props.exercises}</p> 
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div id='content'>
+      <Part part={props.part[0]} exercises={props.exercises[0]}/>
+      <Part part={props.part[1]} exercises={props.exercises[1]}/>
+      <Part part={props.part[2]} exercises={props.exercises[2]}/>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  console.log(props)
+  return (
+  <p>
+    Number of exercises {props.total}
+  </p>
+  )
+}
+
+function App() {
+  const course = 'Half Stack application development'
+  const part1 = 'fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const parts = [part1, part2, part3]
+  const exercises = [exercises1, exercises2, exercises3]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={parts} exercises={exercises}/>
+      <Total total={exercises1 + exercises2 + exercises3}/>
+    </div>
+  )
+}
+
+export default App
