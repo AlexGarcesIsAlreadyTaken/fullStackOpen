e36b1d39c74938285bb041e83d403e77ff89a9f4
AlexGarcesIsAlreadyTaken <alex.garces@estudiantat.upc.edu>
2024-01-29 19:06:40 +0100

    exercice 1.12

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,61 +1,30 @@
 import { useState } from "react"
 
-const Button = ({handleClick, text}) => {
-  return (<button onClick={handleClick}>{text}</button>)
-}
-
-const StatisticLine = ({text, value}) => {
-  return (<tr><td>{text}</td> <td>{value}</td></tr>)
-}
-
-const Statistics = ({good, neutral, bad}) => {
-  const total = good + neutral + bad
-  const average = () => {
-    if (total != 0) return (good - bad)/total
-    return 0
-  }
-
-  const positive = () => {
-    if (total != 0) return good/total
-    return 0
-  }
-
-  if (good != 0 || bad != 0 || neutral != 0) return (
-  <table>
-      <tbody>
-      <StatisticLine text='good' value={good}/>
-      <StatisticLine text='neutral' value={neutral}/>
-      <StatisticLine text='bad' value={bad}/>
-      <StatisticLine text='all' value={total}/>
-      <StatisticLine text='average' value={average()}/>
-      <StatisticLine text='positive' value={positive()*100 + '%'}/>
-      </tbody>
-    </table>
-  )
-  return <>No feedback given</>
-}
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
+  const [selected, setSelected] = useState(0)
+
+  const getRandomInt = (max) => {
+    const randomInt = Math.floor(Math.random() * max)
+    console.log(randomInt)
+    return randomInt
+  }
 
   return (
-    <div>
-      <div>
-        <h1>give feedback</h1>
-        <Button handleClick={() => setGood(good + 1)} text='good'/>
-        <Button handleClick={() => setNeutral(neutral + 1)} text='neutral'/>
-        <Button handleClick={() => setBad(bad + 1)} text='bad'/>
-      </div>
-      <div>
-        <h1>statistics</h1>
-        <Statistics good={good} neutral={neutral} bad={bad} />
-      </div>
-
-
-    </div>
+  <div>
+    <div>{anecdotes[selected]}</div>
+    <button onClick={() => setSelected(getRandomInt(anecdotes.length))}>next anecdote</button>
+  </div>
   )
 }
 
