be2e2f7049e84bffa3a1ee540b1cb6994c755697
AlexGarcesIsAlreadyTaken <alex.garces@estudiantat.upc.edu>
2024-01-30 22:43:25 +0100

    exercise 2.11

--- part2/src/App.jsx
+++ part2/src/App.jsx
@@ -1,18 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Persons from './components/Persons'
 import PersonForm from './components/PersonForm'
 import Phonebook from './components/Phonebook'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [filterName, setFilterName] = useState('')
+
+  useEffect(
+    () => {
+      axios.get('http://localhost:3001/persons').then(
+        response => {setPersons(response.data)}
+      )
+    }
+  , [])
 
   const addNumber = (event) => {
     event.preventDefault()
@@ -20,7 +24,6 @@ const App = () => {
       setPersons(persons.concat({name: newName, number: newNumber, id:persons.length + 1}))
     else alert(newName + ' is already added to phonebook')
   }
-
   const filterHandler = (event) => setFilterName(event.target.value) 
   const handleName = (event) => setNewName(event.target.value)
   const handleNumber = (event) => setNewNumber(event.target.value)
